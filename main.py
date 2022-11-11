031ae48c557ee3dbd26f73f607dc370ddc561c7f
Clayton Safranek <claytonsafranek@gmail.com>
2022-11-11 17:04:10 -0600

    Added "hey assistant" awake check

--- main.py
+++ main.py
@@ -22,27 +22,37 @@ while 1:
 
             r.adjust_for_ambient_noise(source1, duration=0.5)
             audio1 = r.listen(source1)
+            awakeText = r.recognize_google(audio1)
+            awakeText = awakeText.lower()
 
-            inputText = r.recognize_google(audio1)
-            inputText = inputText.lower()
+            # "hey assistant check" - user wants to search something
+            if str(awakeText) == "hey assistant":
+                print("Please say your command...")
 
-            print("Did you say " + inputText)  # This line is here just so you can see what has been interpreted.
+                searchText = r.listen(source1)
 
-            if str(inputText) == "end program":
-                raise SystemExit()
-            print("Respond with either 'YES' or 'NO")
+                inputText = r.recognize_google(searchText)
+                inputText = inputText.lower()
+
+                print("Did you say: \"" + inputText + "\"")  # This line is here just so you can see what has been interpreted.
+
+                if str(inputText) == "end program":
+                    raise SystemExit()
+                print("Respond with either 'YES' or 'NO")
 
 
-            audio2 = r.listen(source1)
-            inputVerify = r.recognize_google(audio2)
-            inputVerify = inputVerify.lower()
-            print(inputVerify)
-            if str(inputVerify) == "yes":
-                SpeakText(inputText)
-                search(inputText)
+                audio2 = r.listen(source1)
+                inputVerify = r.recognize_google(audio2)
+                inputVerify = inputVerify.lower()
+                print(inputVerify)
+                if str(inputVerify) == "yes":
+                    SpeakText(inputText)
+                    search(inputText)
 
     except sr.RequestError as e:
         print("Couldn't recognize results; {0}".format(e))
     except sr.UnknownValueError:
-        print("Unknown error occurred")
+        # if the speech recognizer can't recognize something as speech, it gets caught here
+        # print("Unknown error occurred")
+        print("Sorry I didn't get that. Please say \"Hey Assistant\" to search.")
 
